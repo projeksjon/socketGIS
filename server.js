a4cf193f1fb29bcc12494d160147926c354ef8c7
Ruben Schmidt Mällberg <rubschmi@gmail.com>
2016-02-01 15:33:54 +0100

    removed render that caused error

--- server.js
+++ server.js
@@ -40,13 +40,6 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
     //Connect to dev db.
     mongoose.connect('mongodb://localhost:27017/gisdb');
 }
@@ -54,11 +47,7 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+
 });
 
 
