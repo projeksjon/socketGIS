3171c3d56c7d0c81e23da95166341b863389392d
Valdemar Rolfsen <valdemar.rolfsen@gmail.com>
2016-04-10 15:44:45 +0200

    Added names to messages

--- public/javascripts/angular/app.js
+++ public/javascripts/angular/app.js
@@ -1,4 +1,4 @@
-var socketGis = angular.module("socketGis", ['ngRoute', 'ngCookies', 'btford.socket-io', 'ngFileUpload', 'leaflet-directive', 'angular-jwt']);
+var socketGis = angular.module("socketGis", ['ngRoute', 'ngCookies', 'btford.socket-io', 'ngFileUpload', 'leaflet-directive', 'angular-jwt', 'luegg.directives']);
 
 socketGis.config(function($routeProvider) {
     $routeProvider
